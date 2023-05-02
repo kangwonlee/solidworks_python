c7e227a65189d5fc27e075918f7695857096b7a4
Kangwon Lee (Education) <kangwonlee@users.noreply.github.com>
2023-05-02 13:38:00 +0900

    second attempt
    still error

--- solid.py
+++ solid.py
@@ -7,11 +7,18 @@ swApp = win32com.client.Dispatch("SldWorks.Application")
 # Open a new part document
 swModel = swApp.NewDocument("Part", 0, 0, 0)
 
-# Get the active sketch
+# Create a new sketch plane
+swSketchPlane = swModel.SketchManager.InsertSketch(True)
+
+# Get the sketch object
 swSketch = swModel.SketchManager.ActiveSketch
 
-# Create a new sketch circle
+# Draw a circle in the sketch
 swCircle = swSketch.CreateCircle(0, 0, 0, 0.01)
+
+# Exit the sketch
+swModel.ClearSelection2(True)
+swModel.SketchManager.InsertSketch(False)
 
 # Create an extruded boss feature from the sketch
 swExtrude = swModel.FeatureManager.FeatureExtrusion2(True, False, False, 0, 0, 0.01, 0.01, False, False, False, False, 0, 0, False, False, False, False, True, True, True, 0, 0, False)
